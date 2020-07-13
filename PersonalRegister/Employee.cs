16d949d9c860bfadbc2e6861834cd6b724b72bd4
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-13 10:36:38 +0200

    Changed Id and Salary to be nullable types.

--- PersonalRegister/Employee.cs
+++ PersonalRegister/Employee.cs
@@ -4,15 +4,16 @@ namespace PersonalRegister
 {
     public class Employee
     {
-        public Guid Id { get; set; }
+        //Guid is a value type
+        public Guid? Id { get; set; }
         public string Name { get; set; }
-        public int Salary { get; set; }
+        public int? Salary { get; set; }
 
-        public Employee() : this(null, 0)
+        public Employee() : this(null, null)
         {
         }
 
-        public Employee(string name, int salary)
+        public Employee(string name, int? salary)
         {
             Id = Guid.NewGuid();
             Name = name;
