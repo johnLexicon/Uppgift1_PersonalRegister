65649198adab89dd5fbba45faa25e41e6884b647
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-13 14:43:17 +0200

    Implemented tests for Register class.

--- PersonalRegister.Tests/RegisterShould.cs
+++ PersonalRegister.Tests/RegisterShould.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Xunit;
 
 namespace PersonalRegister.Tests
@@ -5,13 +6,24 @@ namespace PersonalRegister.Tests
     public class RegisterShould
     {
         [Fact]
-        public void TestName()
+        public void CreateEmptyCollectionOfEmployees()
+        {
+            //When
+            var sut = new Register();
+            //Then
+            Assert.NotNull(sut.Employees);
+        }
+
+        [Fact]
+        public void AddAnEmployee()
         {
             //Given
-
+            var employee = new Employee();
+            var sut = new Register();
             //When
-
+            sut.AddEmployee(employee);
             //Then
+            Assert.Equal(employee, sut.Employees.FirstOrDefault());
         }
     }
 }
