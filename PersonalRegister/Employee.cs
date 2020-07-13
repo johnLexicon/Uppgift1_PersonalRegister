7fce876fcd7c7ee587dd927683dfb97f90dbd7b9
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-13 09:17:38 +0200

    Added constructors.

--- PersonalRegister/Employee.cs
+++ PersonalRegister/Employee.cs
@@ -8,6 +8,17 @@ namespace PersonalRegister
         public string Name { get; set; }
         public int Salary { get; set; }
 
+        public Employee() : this(null, 0)
+        {
+        }
+
+        public Employee(string name, int salary)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Salary = salary;
+        }
+
         public override string ToString()
         {
             return $"Id: {Id}, Name: {Name}, Salary: {Salary}";
