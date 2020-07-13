2f4913723437f306007cbf6f3e00a57cafe313eb
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-13 10:37:31 +0200

    Implemented tests for Employee class.

--- PersonalRegister.Tests/EmployeeShould.cs
+++ PersonalRegister.Tests/EmployeeShould.cs
@@ -5,6 +5,29 @@ namespace PersonalRegister.Tests
 {
     public class EmployeeShould
     {
+        [Fact]
+        public void CreateEmployeeWithOnlyId()
+        {
+            //Act
+            var sut = new Employee();
+            //Assert
+            Assert.Null(sut.Name);
+            Assert.Null(sut.Salary);
+            Assert.NotNull(sut.Id);
+        }
 
+        [Fact]
+        public void CreateEmployeeWithNameAndSalaryAndId()
+        {
+            //Arrange
+            var name = "Kalle Anka";
+            var salary = 30000;
+            //Act
+            var sut = new Employee(name, salary);
+            //Assert
+            Assert.Equal(name, sut.Name);
+            Assert.Equal(salary, sut.Salary);
+            Assert.NotNull(sut.Id);
+        }
     }
 }
